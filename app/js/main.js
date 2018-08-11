f9404dcc5eef5a55dd910dc7bc91f3f8761e01c1
Leonid Dobryvechir <leo.dobrivecher@gmail.com>
2018-08-12 00:50:45 +0300

    the slider is complated

--- app/js/main.js
+++ app/js/main.js
@@ -9,4 +9,47 @@ $('.owl-carousel').owlCarousel({
             items:2
         }
     }
-})
+});
+var width = $(window).width();
+
+if(width > 767){
+    $( "#slide1" ).click(function(){
+        $('.slides-box').animate({
+              scrollLeft: '0',
+          }, 500);
+    });
+    $( "#slide2" ).click(function(){    
+      $('.slides-box').animate({
+              scrollLeft: '300',
+          }, 500);
+    });
+    $( "#slide3" ).click(function(){
+          $('.slides-box').animate({
+              scrollLeft: '600',
+          }, 500);
+    });
+}else {
+    $( "#slide1" ).click(function(){
+        $('.slides-box').animate({
+              scrollLeft: '0',
+          }, 500);
+    });
+    $( "#slide2" ).click(function(){    
+      $('.slides-box').animate({
+              scrollLeft: '247',
+          }, 500);
+    });
+    $( "#slide3" ).click(function(){
+          $('.slides-box').animate({
+              scrollLeft: '500',
+          }, 500);
+    });
+}
+console.log(width);  
+ 
+ 
+
+  $('.slider-nav-link').click(function(){
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+  });
