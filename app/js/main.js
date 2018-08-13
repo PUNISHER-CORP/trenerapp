01da702bcdb0dd12b08b62d553862cfe4b7bf4ce
Leonid Dobryvechir <leo.dobrivecher@gmail.com>
2018-08-13 12:50:54 +0300

    fixed slider nav

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,38 @@
-$('#owl-carousel2').owlCarousel({
-    loop:true,
-    margin:10,
-    responsive:{
-        320:{
-            items:1
-        },
-        1024:{
-            items:2
+
+$( document ).ready(function() {
+    $('#owl-carousel2').owlCarousel({
+        loop:true,
+        margin:10,
+        responsive:{
+            320:{
+                items:1
+            },
+            1024:{
+                items:2
+            }
         }
-    }
-});
-
-var carousel = $('#owl-carousel1').owlCarousel({
-    loop:false,
-    nav:false,
-    items:1,
-    dots:false
-});
-
-
-$('.slider-nav-link').click(function(){
-    $('.active').removeClass('active');
-    $(this).addClass('active');
-});
-
-$('.slider-nav-link').click( function () {
-    var click = $(this).index();
-    carousel.trigger( 'to.owl.carousel', [click] );
+    });
+    
+    var carousel = $('#owl-carousel1').owlCarousel({
+        loop:false,
+        nav:false,
+        items:1,
+        dots:false
+    });
+    
+    
+    $('.slider-nav-link').click( function () {
+        var click = $(this).index();
+        carousel.trigger( 'to.owl.carousel', [click] );
+    });
+    
+    carousel.on('changed.owl.carousel', function(e) {
+       activeItem = e.item.index;
+       if(activeItem >=0){
+            $('.active').removeClass('active');
+            $('p#'+activeItem).addClass('active');
+       }
+    });
+      
+    
 });
